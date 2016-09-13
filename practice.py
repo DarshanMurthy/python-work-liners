ad317e2609bfad9679f0ee5331aeff8e275d5a49
DarshanMurthy <darshants.darshan1@gmail.com>
2016-09-13 15:59:32 -0700

    first commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+class Test(object):
+	def __init__(self,name, place):
+		self.name = name
+		self.place = place
+
+	def display(self):
+		return self.name 
+
+
+
+import itertools 
+
+
+
+
---
+++ routines.py
@@ -0,0 +1,35 @@
+class Test(object):
+	def __init__(self,name=None,id=None):
+		self.name = name 
+		self.id = id
+
+	def display(self):
+		return self.id
+
+test = Test("Darshan","12") 
+print(test.displays())
+
+class Service(object):
+	def __init__(self,host,binary,topic,manager,report_interval=None,periodic_interval=None, *args, **kwargs):
+		print('Initializing Service')
+		super(Service,self).__init__(*args,**kwargs)
+
+class Color(object):
+	def __init__(self, color='red', **kwargs):
+		print('Initializing Color')
+		self.color = color
+		super(Color, self).__init__(**kwargs)
+
+class ColoredService(Service,Color):
+	def __init__(self, *args, **kwds):
+		print('Initializing Colored Service')
+		super(ColoredService,self).__init__(*args, **kwds)
+
+c = ColoredService('host','bin','top','mgr','ivl', color='blue')
+
+
+
+
+
+
+
